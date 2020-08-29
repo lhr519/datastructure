580a9d3a9f7a0b962d48b25e00ad1b17dad48f2e
lhr519 <lhr519@naver.com>
2020-08-29 20:25:48 +0900

    test
    test

---
+++ test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by 이해린 on 2020/08/29.
+//  Copyright © 2020 이해린. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
